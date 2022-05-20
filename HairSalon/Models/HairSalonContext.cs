5e8180389db1edfd349f3787ff6d3daca39e1a6a
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-20 09:20:17 -0700

    add mySQL database, Stylists View workflow

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    // public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
     // public DbSet<Category> Categories { get; set; }
     public HairSalonContext(DbContextOptions options) : base(options) { }
 
