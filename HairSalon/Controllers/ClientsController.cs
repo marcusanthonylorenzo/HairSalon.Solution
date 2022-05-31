2fd96a746e28251fd66b180b5684d39301303d24
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-31 00:06:11 -0700

    forgot to save and submit? will ask tomorrow

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -4,35 +4,46 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-    private readonly HairSalonContext _db1;
+    private readonly HairSalonContext _db;
 
     public ClientsController(HairSalonContext dbClient)
     {
-      _db1 = dbClient;
+      _db = dbClient;
     }
 
      public ActionResult Index()
     {
-      List<Client> model = _db1.Clients.Include(client =>  client.Stylist).ToList();
+      // List<Client> model = _db.Clients.Include(client =>  client.ClientId).ToList();
+      // return View(model);
+      List<Client> model = _db.Clients.ToList();
       return View(model);
     }
 
     public ActionResult Add()
     {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
     [HttpPost]
     public ActionResult Add(Client client)
     {
-      _db1.Clients.Add(client);
-      _db1.SaveChanges();
-      return RedirectToAction("SelectStylist", "Stylists");
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+      Client currentClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+      return View(currentClient);
+    }
+
   }
 }
 
